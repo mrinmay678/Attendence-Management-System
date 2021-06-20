3b8b36ffe5c2f163e9d6fac171489cf1bdd57fa7
mrinmay678 <mrinmay678@gmail.com>
2021-06-20 18:00:58 +0530

    m

---
+++ LMS/authentication/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils import timezone
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, jis_id, password, **extra_fields):
+        if not email:
+            raise ValueError('Provide Your ID')
+        user = self.model(jis_id=jis_id, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, jis_id, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(jis_id, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    jis_id = models.CharField(max_length=20, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'jis_id'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.jis_id
