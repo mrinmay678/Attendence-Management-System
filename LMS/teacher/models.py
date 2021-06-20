9ff2d71d106052c0073e7fc544e6fed4cc851396
mrinmay678 <61239380+mrinmay678@users.noreply.github.com>
2021-06-20 18:09:27 +0530

    Delete LMS directory

--- LMS/teacher/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from authentication.models import CustomUser
-
-class Teacher(models.Model):
-    user=models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    name=models.CharField(max_length=255,blank=False)
-    email=models.EmailField(blank=False)
-    phone=models.CharField(max_length=255,blank=False)
-    department=models.CharField(max_length=30,blank=False)
